ae2486dc84d833f13f84cacaa8edd081ee193548
Juan <jasobrino66@gmail.com>
2018-11-16 11:38:02 +0100

    modificaciones

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE    = 'static-v2';
+const STATIC_CACHE    = 'static-v3';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -63,12 +63,14 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
 
+  if( e.request.url.includes('img/'))
+    console.log('request:', e.request.url);
   const respuesta = caches.match( e.request ).then( res => {
 
     if( res ) {
       return res;
     } else {
-      
+
       return fetch( e.request ).then( newRes => {
         return actulizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
       });
@@ -78,4 +80,5 @@ self.addEventListener('fetch', e => {
 
 
   e.respondWith( respuesta );
-})
+});
+
